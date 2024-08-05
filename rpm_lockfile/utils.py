f04c8ab628f9a3402aa28d92dbbed1483178cd94
Lubomír Sedlář <lsedlar@redhat.com>
2024-08-05 09:11:39 +0200

    Detect Containerfile or Dockerfile
    This allows users to follow whatever naming convention they prefer.

--- rpm_lockfile/utils.py
+++ rpm_lockfile/utils.py
@@ -6,3 +6,14 @@ def relative_to(directory, path):
     if path:
         return os.path.join(directory, path)
     return None
+
+
+def find_containerfile(dir):
+    """Look for a containerfile in the given directory.
+
+    Returns a path to the found file or None.
+    """
+    for candidate in (dir / "Containerfile", dir / "Dockerfile"):
+        if candidate.exists():
+            return candidate
+    return None
