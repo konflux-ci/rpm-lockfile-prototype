f1da16753cf8014aec6d92f966aebac80831e04d
Lubomír Sedlář <lsedlar@redhat.com>
2024-08-29 09:56:14 +0200

    Add label interpolation to repo baseurl
    Fixes: #36

--- rpm_lockfile/content_origin/repos.py
+++ rpm_lockfile/content_origin/repos.py
@@ -1,4 +1,7 @@
+import os
+
 from . import Repo
+from .. import utils
 
 
 class RepoOrigin:
@@ -7,12 +10,24 @@ class RepoOrigin:
         "properties": {
             "repoid": {"type": "string"},
             "baseurl": {"type": "string"},
+            "varsFromImage": {"type": "string"},
+            "varsFromContainerfile": {"type": "string"},
         },
         "required": ["repoid", "baseurl"],
     }
 
-    def __init__(self, *args, **kwargs):
-        pass
+    def __init__(self, config_dir):
+        self.config_dir = config_dir
 
     def collect(self, sources):
-        yield from (Repo.from_dict(s) for s in sources)
+        for source in sources:
+            image = source.pop("varsFromImage", None)
+            containerfile = source.pop("varsFromContainerfile", None)
+            vars = utils.get_labels(image, self._get_container_file(containerfile))
+            source["baseurl"] = utils.subst_vars(source["baseurl"], vars)
+            yield Repo.from_dict(source)
+
+    def _get_container_file(self, containerfile):
+        if containerfile:
+            return os.path.join(self.config_dir, containerfile)
+        return None
