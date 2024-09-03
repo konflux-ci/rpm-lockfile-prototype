e4a9c3ee8d20bc3d53ac107ee40abd959f288542
Lubomír Sedlář <lsedlar@redhat.com>
2024-09-03 10:58:32 +0200

    Move tests for removing tags in favor of digests
    The code is now in utils module, move the test to the corresponding
    part. A new test case is added to ensure the stripping is called when
    extracting labels.

--- tests/test_containers.py
+++ tests/test_containers.py
@@ -9,19 +9,7 @@ from unittest import mock
 
 import pytest
 
-from rpm_lockfile import containers, utils
-
-
-@pytest.mark.parametrize(
-    "image_spec,expected",
-    [
-        ("example.com/image:latest", "example.com/image:latest"),
-        ("example.com/image@sha256:abcdef", "example.com/image@sha256:abcdef"),
-        ("example.com/image:latest@sha256:0123456", "example.com/image@sha256:0123456"),
-    ],
-)
-def test_strip_tag(image_spec, expected):
-    assert utils.strip_tag(image_spec) == expected
+from rpm_lockfile import containers
 
 
 @pytest.fixture
